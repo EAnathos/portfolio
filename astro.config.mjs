019edab2210f9affca6672934f14a567bc24fa4e
EAnathos <83123402+EAnathos@users.noreply.github.com>
2026-02-14 23:36:00 +0100

    feat: update email contact links to use a mailto format

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
-    server: {
+    preview: {
       allowedHosts: ['anathos.me']
     }
   }
